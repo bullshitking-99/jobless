904a86ba6e7b45314341092d7d46541ec2e6a2cd
李名河 <2219845592@qq.com>
2023-09-10 22:00:34 +0800

    update

--- app/crawler/index.cjs
+++ app/crawler/index.cjs
@@ -7,44 +7,59 @@ const https = require("https");
 const cheerio = require("cheerio");
 const fs = require("fs");
 
-//获取页面的html结构
-https.get("https://movie.douban.com/top250?start=25", function (res) {
-  let html = "";
+// 总数组
+// let topMovies = [];
 
-  res.on("data", function (chunk) {
-    html += chunk;
+//单次获取
+function getTopMovies(page) {
+  return new Promise((resolve, reject) => {
+    try {
+      https.get(
+        `https://movie.douban.com/top250?start=${page}`,
+        function (res) {
+          let html = "";
+
+          res.on("data", function (chunk) {
+            html += chunk;
+          });
+
+          res.on("end", function () {
+            // 获取html中的数据
+            const $ = cheerio.load(html);
+            let allFiles = [];
+            //拿到每一个item中我们需要的数据
+            $("li .item").each(function () {
+              const title = $(".title", this).text();
+              const star = $(".info .bd .rating_num", this).text();
+              const pic = $(".pic img", this).attr("src");
+              //数据以对象的形式存放在数组中
+              allFiles.push({
+                title: title,
+                star: star,
+                pic: pic,
+              });
+            });
+
+            resolve(allFiles);
+          });
+        }
+      );
+    } catch (error) {
+      reject(error);
+    }
   });
+}
 
-  res.on("end", function () {
-    fs.writeFile(
-      __dirname + "/assets/html.json",
-      JSON.stringify(html),
-      (err) => {
-        if (err) console.log(err);
-        console.log("html 输出成功");
-      }
-    );
+// 根据pages创建任务数组
 
-    // 获取html中的数据
-    const $ = cheerio.load(html);
-    let allFiles = [];
-    //拿到每一个item中我们需要的数据
-    $("li .item").each(function () {
-      const title = $(".title", this).text();
-      const star = $(".info .bd .rating_num", this).text();
-      const pic = $(".pic img", this).attr("src");
-      //数据以对象的形式存放在数组中
-      allFiles.push({
-        title: title,
-        star: star,
-        pic: pic,
-      });
-    });
-    //console.log(allFiles);
+const tasks = new Array(10).fill(0).map((_, index) => getTopMovies(index * 25));
+
+Promise.all(tasks)
+  .then((val) => {
     //将数据写入文件中
     fs.writeFile(
       __dirname + "/assets/data.json",
-      JSON.stringify(allFiles),
+      JSON.stringify(val),
       function (err) {
         if (err) {
           throw err;
@@ -52,8 +67,10 @@ https.get("https://movie.douban.com/top250?start=25", function (res) {
         console.log("文件保存成功");
       }
     );
+  })
+  .catch((err) => {
+    console.log(err);
   });
-});
 
 // eslint-disable-next-line no-undef
 // module.exports = https;
