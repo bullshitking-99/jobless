18b2dd2c539334cd72e42dd402ffd98bb8498067
李名河 <2219845592@qq.com>
2023-09-11 21:31:39 +0800

    arrange

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import Plums from "./pages/plums";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Plums />
   </React.StrictMode>
 );
