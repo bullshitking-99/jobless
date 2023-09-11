6b9cb8c6d644c469fba3129c4ed77b2d8a024998
李名河 <2219845592@qq.com>
2023-09-11 21:39:03 +0800

    init movies

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Plums from "./pages/plums";
+import App from "./pages/movies";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Plums />
+    <App />
   </React.StrictMode>
 );
