621c805c6176babe34b57223d479e63342eb8948
李名河 <2219845592@qq.com>
2023-09-16 13:27:16 +0800

    add dynamic variants

--- src/pages/movies/movies.tsx
+++ src/pages/movies/movies.tsx
@@ -6,19 +6,38 @@ function App() {
   // 取初始数据
   const data = movies[0];
 
+  // 动画状态切换
+
+  const info = {
+    visible: (i: number) => ({
+      opacity: 1,
+      x: 0,
+      transition: {
+        delay: i * 0.3,
+      },
+    }),
+    hidden: { opacity: 0, x: 100 },
+  };
+
   return (
     <div className="container">
-      {data.map((m) => (
-        <div key={m.title} className="movie">
+      {data.map((m, i) => (
+        <motion.div key={m.title} className="movie">
           <div className="img">
             <motion.img drag dragSnapToOrigin src={m.pic} alt={m.title} />
           </div>
-          <div className="info">
+          <motion.div
+            className="info"
+            custom={i}
+            variants={info}
+            initial="hidden"
+            animate="visible"
+          >
             <div className="title">{m.title} </div>
             <div className="star">{m.star} </div>
             <div className="inq">{m.inq} </div>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       ))}
     </div>
   );
