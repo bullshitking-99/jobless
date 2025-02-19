4feff9a0f55c839c3cd932c8b4d95b835df6c66a
李名河 <2219845592@qq.com>
2025-02-19 19:09:56 +0800

    feat: 树转数组

--- src/pages/CodeTest/ArrayToTree.tsx
+++ src/pages/CodeTest/ArrayToTree.tsx
@@ -1,37 +1,35 @@
 import { Button } from "antd";
 
 // 测试数据
-const data = [
+const dataArr = [
   { id: 1, parentId: 0, name: "A" },
-  { id: 22121, parentId: 1, name: "B" },
-  { id: 32121, parentId: 1, name: "C" },
-  { id: 12121, parentId: 22121, name: "D" },
+  { id: 2, parentId: 1, name: "B" },
+  { id: 3, parentId: 1, name: "C" },
+  { id: 4, parentId: 2, name: "D" },
 ];
 
 // 目标输出
-// {
-//     id: 1,
-//     parentId: 0,
-//     name: 'A',
-//     children: [
-//       {
-//         id: 2,
-//         parentId: 1,
-//         name: 'B',
-//         children: [
-//           { id: 4, parentId: 2, name: 'D' }
-//         ]
-//       },
-//       { id: 3, parentId: 1, name: 'C' }
-//     ]
-//   }
+const dataTree = {
+  id: 1,
+  parentId: 0,
+  name: "A",
+  children: [
+    {
+      id: 2,
+      parentId: 1,
+      name: "B",
+      children: [{ id: 4, parentId: 2, name: "D" }],
+    },
+    { id: 3, parentId: 1, name: "C" },
+  ],
+};
 
 const ArrayToTree = () => {
-  const transfer = () => {
+  const Arr2Tree = () => {
     let root = null;
     const map = new Map();
 
-    data.forEach((c) => {
+    dataArr.forEach((c) => {
       if (c.parentId === 0) root = c;
       map.set(c.id, c);
     });
@@ -50,15 +48,42 @@ const ArrayToTree = () => {
     return root;
   };
 
+  const Tree2Arr = () => {
+    const arr = [];
+    const queue = [dataTree];
+
+    while (queue.length) {
+      const node = queue.shift();
+      const { children, ...rest } = node;
+      arr.push(rest);
+      if (children && children.length) {
+        children.forEach((c) => {
+          queue.push(c);
+        });
+      }
+    }
+
+    return arr;
+  };
+
   return (
     <div>
       <Button
         type="primary"
         onClick={() => {
-          console.log(transfer());
+          console.log(Arr2Tree());
         }}
       >
-        transfer
+        Arr2Tree
+      </Button>
+      {"    "}
+      <Button
+        type="primary"
+        onClick={() => {
+          console.log(Tree2Arr());
+        }}
+      >
+        Tree2Arr
       </Button>
     </div>
   );
